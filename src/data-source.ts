d2ae5a13c1072f2d3006bc9a5534b1ad0eacb765
GustavoKntsu <gustavokntsu@gmail.com>
2026-08-27 18:03:28 -0300

    Fianalizando a 1º aula: adicionando as ultimas dependências

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+    const dialect = process.env.DB_DIALECT ?? "postgres";
+export const AppDataSource = new DataSource({
+    type: dialect as "postgres" | "mysql" | "mariadb" | "mongodb",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: false,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+})
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+//Importação do express
 import express from "express";
+//Importar as variaveis de ambiente
+import dotenv from "dotenv";
+//Carregar as variáveis
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +12,6 @@ import login from "./controllers/login";
 //Criação da rota para o controller login
 app.use("/", login);
 
-app.listen(8080, () => {
-  console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor iniciado na porta ${process.env.PORT}: http://localhost:${process.env.PORT}`);
 });
